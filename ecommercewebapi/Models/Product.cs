320f00c77f6713315a7511e936db309d172aa8a5
Adrian <adr1337@outlook.com>
2026-01-07 17:28:19 -0300

    subida de proyecto con modelo producto

---
+++ ecommercewebapi/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace ecommercewebapi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string Code { get; set; }
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+        }
+}
